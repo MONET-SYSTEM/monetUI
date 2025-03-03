2ef024c46aa34834a36fe707e739c34a60eaf2f3
Marjovic <marjovicalejado123.com>
2025-03-03 22:35:52 +0800

    Class for colors

--- lib/resources/app_colours.dart
+++ lib/resources/app_colours.dart
@@ -3,5 +3,10 @@ import 'dart:ui';
 class AppColours {
   static Color primaryColour = const Color(0xff0077FF);
   static Color primaryColourLight = const Color(0xffBDDCFF);
+
+  static Color backgroundColor = const Color(0xffFFFFFF);
+  static Color light20 = const Color(0xff91919F);
+  static Color dark100 = const Color(0xff0D0E0F);
+
 }
 
