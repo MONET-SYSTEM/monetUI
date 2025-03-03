ca7b0d84e06dab0ab7684be94635f19a842911fc
Marjovic <marjovicalejado123.com>
2025-03-03 19:15:03 +0800

    Implementing WalkThroughScreen for next UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:monet/resources/app_colours.dart';
+import 'package:monet/resources/app_routes.dart';
 import 'package:monet/resources/app_strings.dart';
 import 'package:monet/resources/views/onboarding/splash_screen.dart';
+import 'package:monet/resources/views/onboarding/walkthrough.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +20,10 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: AppColours.primaryColour),
       ),
-     initialRoute: '/',
+     initialRoute: AppRoutes.splash,
      routes: {
-        '/': (context)=> const SplashScreen()
+        AppRoutes.splash: (context)=> const SplashScreen(),
+       AppRoutes.walkThrough: (context)=> const WalkthroughScreen()
      },
     );
   }
--- lib/resources/views/onboarding/splash_screen.dart
+++ lib/resources/views/onboarding/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:monet/resources/app_colours.dart';
+import 'package:monet/resources/app_routes.dart';
+import 'package:monet/resources/app_strings.dart';
+import 'package:monet/resources/app_styles.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +14,23 @@ class SplashScreen extends StatefulWidget {
 class _State extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColours.primaryColour,
+      body: Center(
+        child: Text(AppStrings.appName, style: AppStyles.titleX(size: 56, color: Colors.white),),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    initApp();
+    super.initState();
+  }
+
+  void initApp() {
+    Future.delayed(
+        const Duration(seconds: 5),
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.walkThrough));
   }
 }
