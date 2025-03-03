41617f9155cae0480b0490f5de5d9b5dd2f39b1b
Marjovic <marjovicalejado123.com>
2025-03-03 22:36:06 +0800

    Class for strings text

--- lib/resources/app_strings.dart
+++ lib/resources/app_strings.dart
@@ -1,3 +1,7 @@
 class AppStrings {
   static String appName = 'Monet';
+  static String signUp = 'Sign Up';
+  static String logIn = 'Log In';
+  static String walkthroughTitle1 = "Gain total control of your money";
+  static String walkthroughDescription1 = "Become your own money manager and make every cent count";
 }
