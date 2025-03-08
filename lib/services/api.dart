cc67516d20ae950bb5d91843bfa70f2113b5f82a
Marjovic <marjovicalejado123.com>
2025-03-08 22:21:44 +0800

    Add ApiService class for HTTP requests

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,13 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:monet/resources/app_strings.dart';
+import 'package:monet/services/auth_service.dart';
 
 class ApiService {
-
   static final dio = Dio();
 
   // post
   static Future<Response> post(String url, Map<String, dynamic> body) async {
+    final user = await AuthService.get();
     final response = await dio.post(url, data: body, options: Options(headers: {
-      'Accept': 'application/json'
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${user?.token}'
     }));
 
     return response;
@@ -15,11 +18,25 @@ class ApiService {
 
   // get
   static Future<Response> get(String url, Map<String, dynamic> params) async {
+    final user = await AuthService.get();
     final response = await dio.get(url, queryParameters: params, options: Options(headers: {
-      'Accept': 'application/json'
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${user?.token}'
     }));
 
     return response;
   }
 
+  static String errorMessage(DioException dioException) {
+    final internetErrors = [
+      DioExceptionType.connectionError,
+      DioExceptionType.connectionTimeout,
+      DioExceptionType.sendTimeout,
+      DioExceptionType.receiveTimeout
+    ];
+
+    return (internetErrors.contains(dioException.type))
+        ? AppStrings.noInternetAccess
+        : (dioException.response?.data['message'] ?? AppStrings.anErrorOccurredTryAgain);
+  }
 }
